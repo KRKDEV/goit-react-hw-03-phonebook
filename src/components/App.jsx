bd2e5b5832b380d7936dab4f9000fe8e64bdeaaf
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-04-23 18:41:40 +0200

    Added files from HW#2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import css from './App.module.css';
+import { useState } from 'react';
+import Form from './Form/Form';
+import Filter from './Filter/Filter';
+import ContactList from './Contacts/Contacts';
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const addContact = contact => {
+    setContacts(prevContacts => [contact, ...prevContacts]);
+  };
+
+  const filterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  const filterContacts = () => {
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+  const filteredContacts = filterContacts();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <Form contacts={contacts} onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={filterChange} />
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
     </div>
   );
 };
